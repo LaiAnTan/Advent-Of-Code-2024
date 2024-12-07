94bd51d273ff2ad9caea4bf16fc887dd7afe8a03
LaiAnTan <tanlaian2004@gmail.com>
2024-12-07 14:31:27 +0800

    day07

---
+++ day07/day07_part1.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+template <typename S>
+std::ostream &operator << (std::ostream &os, const std::vector<S> &vector)
+{
+	for (auto i : vector)
+		os << i << " ";
+	return os;
+}
+
+bool aux(long target, long curr_val, std::vector<int> components)
+{
+	// base case
+	if (components.empty())
+		return (target == curr_val);
+	
+	int next = components[0];
+
+	components.erase(components.begin());
+	return (aux(target, curr_val * next, components) ||
+			aux(target, curr_val + next, components));
+}
+
+int tryPossibleCombinations(long target, std::vector<int> components)
+{
+	int first = components[0];
+	components.erase(components.begin());
+
+	return (aux(target, first, components));
+}
+
+int main(int argc, char **argv)
+{
+
+	if (argc != 2)
+			return (-1);
+		
+	std::string		filename(argv[1]);
+	std::fstream	data(filename, std::fstream::in);
+	std::string		line;
+
+	if (!data.is_open())
+		return (-1);
+
+	std::vector<std::vector<char> >	layout;
+
+	long long res = 0;
+
+	while (std::getline(data, line))
+	{
+		long target = std::stol(line.substr(0, line.find(':')));
+
+		std::vector<int>	components;
+		std::stringstream ss(line.substr(line.find(":") + 1));
+		int num;
+		while (ss >> num)
+			components.push_back(num);
+
+		std::cout << target << " = " << components << std::endl;
+
+		if (tryPossibleCombinations(target, components))
+			res += target;
+	}
+
+	std::cout << res << std::endl;
+
+	return (0);
+}
